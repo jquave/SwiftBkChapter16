23e0f1feb9f3f85c3f15c0a205ab9fd612ca2912
Jameson Quave <jquave@gmail.com>
2014-11-08 11:37:52 -0600

    Chapter 16

--- LocationServices/ViewController.swift
+++ LocationServices/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var totalDistanceTraveled = 0.0
+    var lastKnownLocation : CLLocation?
+    
+    let locationManager = CLLocationManager()
+    let locationLabel = UILabel(frame: UIScreen.mainScreen().bounds)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationLabel.text = "You can now suspend the app and record distance traveled."
+        locationLabel.textAlignment = .Center
+        locationLabel.numberOfLines = 3
+        self.view.addSubview(locationLabel)
+        
+        startReportingLocation()
+        
+        NSNotificationCenter.defaultCenter().addObserverForName(UIApplicationWillEnterForegroundNotification, object: nil, queue: nil) { (notification) -> Void in
+            let distInFt = round(self.totalDistanceTraveled * 3.28084 * 100.0) / 100.0
+            self.locationLabel.text = "Distance Traveled\n \(distInFt)ft"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func startReportingLocation() {
+        // Set the delegate for the location manager
+        locationManager.delegate = self
+        
+        // Specify we'd like the highest level of accuracy available on this device
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // Ask the user for permission to use their location in the background
+        locationManager.requestAlwaysAuthorization()
+        
+        // Start producing updates
+        locationManager.startUpdatingLocation()
     }
-
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [CLLocation]!) {
+        if let location = locations.first {
+            if let lastLocation = lastKnownLocation {
+                // Find the distance to the previous location
+                let distance = lastLocation.distanceFromLocation(location)
+                totalDistanceTraveled += distance
+                
+                /*
+                // Uncomment this for real-time updates (consumes more battery power, but good for testing)
+                let distInFt = round(self.totalDistanceTraveled * 3.28084 * 100.0) / 100.0
+                self.locationLabel.text = "Distance Traveled\n \(distInFt)ft"
+                */
+            }
+            lastKnownLocation = location
+        }
+    }
 
 }
-
